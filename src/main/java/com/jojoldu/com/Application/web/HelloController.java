877cc3d16deea52b3b3cba0296666a1f27479c1d
sht3898 <sehyunteg@naver.com>
2021-05-11 18:58:39 +0900

    210511 | 복습 - JPA 78p까지 완료

--- src/main/java/com/jojoldu/com/Application/web/HelloController.java
+++ src/main/java/com/jojoldu/com/Application/web/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 줌, 예전에는 @ResponseBody를 각 메소드마다 선언했음
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello")   // HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 줌
     public String hello() {
         return "hello";
     }
