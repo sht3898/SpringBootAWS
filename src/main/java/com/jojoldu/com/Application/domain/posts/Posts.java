cd6cd1f2b34042b66e930beca265e45104785bfe
sht3898 <sehyunteg@naver.com>
2021-05-15 02:45:19 +0900

    210515 | 스프링부트에서 JPA로 데이터베이스 다루기 - p124까지 완료

--- src/main/java/com/jojoldu/com/Application/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/com/Application/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.com.Application.domain.posts;
 
+import com.jojoldu.com.Application.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter // 클래스 내 모든 필드의 Getter 메소드를 자동생성
 @NoArgsConstructor  // 기본 생성자 자동 추가, public Posts(){}와 같은 효과
 @Entity // 테이블과 링크될 클래스임을 나타냄
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // 해당 테이블의 pk 필드
     @GeneratedValue(strategy = GenerationType.IDENTITY) // pk의 생성 규칙을 나타냄, IDENTITY 옵션은 auto_increment
@@ -29,4 +30,10 @@ public class Posts {
         this.content = content;
         this.author = author;
     }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
 }
--- src/main/java/com/jojoldu/com/Application/web/PostsApiController.java
+++ src/main/java/com/jojoldu/com/Application/web/PostsApiController.java
@@ -1,12 +1,11 @@
 package com.jojoldu.com.Application.web;
 
 import com.jojoldu.com.Application.service.posts.PostsService;
+import com.jojoldu.com.Application.web.dto.PostsResponseDto;
 import com.jojoldu.com.Application.web.dto.PostsSaveRequestDto;
+import com.jojoldu.com.Application.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor    // final 이 선언된 모든 필드를 인자값으로 하는 생성자를 생성해 줌
 @RestController
@@ -17,5 +16,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
